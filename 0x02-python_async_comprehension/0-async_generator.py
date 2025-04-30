488279801935120e140e4f5092c7c11e49e291a5
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-04-30 08:42:52 +0300

    Adds async_generator()

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Module 0-async_generator
+
+This module contains an asynchronous generator function `async_generator`
+that yields random floating-point numbers between 0 and 10. The function
+sleeps for 1 second before yielding each number.
+"""
+import random
+import asyncio
+from typing import AsyncGenerator, Generator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Asynchronous generator that yields 10 random floats between 0 and 10.
+
+    Yields:
+        float: A random floating-point number between 0 and 10.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
