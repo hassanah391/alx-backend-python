b8366c221c41d026dba28e0a9166938cb51702d0
Hassan Ahmed <hassan.ahmed3577@gmail.com>
2025-04-30 08:53:25 +0300

    Annottates with Generator instead of AsyncGenerator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,10 +7,10 @@ sleeps for 1 second before yielding each number.
 """
 import random
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Asynchronous generator that yields 10 random floats between 0 and 10.
 
     Yields:
---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python3
+"""Module 1-async_comprehension"""
